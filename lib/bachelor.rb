d2af481f36f2df65e24a2e4b3f1f722d7882a08a
Peter Keller <keller.f.peter@gmail.com>
2019-03-07 18:37:49 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,59 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |sea, person|
+    if sea == season
+      person.each do |item|
+        if item["status"] == "Winner"
+          return item["name"].split(" ")[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |key, value|
+    value.each do |person|
+      if person["occupation"] == occupation
+        return person["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |key, value|
+    value.each do |person|
+      if person["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |key, value|
+    value.each do |person|
+      if person["hometown"] == hometown
+        return person["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum = 0
+  count = 0
+  data.each do |sea, person|
+    if sea == season
+      person.each do |item|
+        sum += item["age"].to_i
+        count += 1
+      end
+    end
+  end
+  return (sum.to_f / count).round
 end
